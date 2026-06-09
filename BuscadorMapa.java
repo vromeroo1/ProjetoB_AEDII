774a2a638ea3e97783ff355523be9677521dfa6e
vromeroo1 <victor.romero.060600@gmail.com>
2026-06-09 20:14:32 -0300

    Encapsula busca na lista duplamente ligada

--- BuscadorMapa.java
+++ BuscadorMapa.java
@@ -1,38 +1,9 @@
 public class BuscadorMapa {
     public LocalMapa buscarMaisProximo(ListaDuplamenteLigada lista, int numeroPesquisado) {
-        if (lista == null || lista.vazia()) {
+        if (lista == null) {
             return null;
         }
 
-        Celula atual = lista.getPrimeira();
-        LocalMapa anterior = null;
-
-        while (atual != null) {
-            LocalMapa localAtual = (LocalMapa) atual.getElemento();
-
-            if (localAtual.getNumero() == numeroPesquisado) {
-                return localAtual;
-            }
-
-            if (localAtual.getNumero() > numeroPesquisado) {
-                if (anterior == null) {
-                    return localAtual;
-                }
-
-                int diferencaAnterior = Math.abs(numeroPesquisado - anterior.getNumero());
-                int diferencaAtual = Math.abs(numeroPesquisado - localAtual.getNumero());
-
-                if (diferencaAnterior <= diferencaAtual) {
-                    return anterior;
-                }
-
-                return localAtual;
-            }
-
-            anterior = localAtual;
-            atual = atual.getProxima();
-        }
-
-        return anterior;
+        return lista.buscarMaisProximo(numeroPesquisado);
     }
 }
